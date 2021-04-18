458b663ace270a973628f3d5bcba9579bbe70898
altalarikyusuf98 <altalarik@gmail.com>
2021-04-18 14:40:26 +0700

    Upload Project Praktikum 10

---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+  export default {
+    apiKey: "AIzaSyBsfZUdY_qRfnF2CUCdgVw98Pm2fmI3YOc",
+    authDomain: "react-firebase-cb814.firebaseapp.com",
+    projectId: "react-firebase-cb814",
+    storageBucket: "react-firebase-cb814.appspot.com",
+    messagingSenderId: "238853318571",
+    appId: "1:238853318571:web:72e2668b16b00fb9eed4ee",
+    measurementId: "G-NTS6H4G3LC"
+  }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {Switch, BrowserRouter as Router, Route} from 'react-router-dom';
+import routes from './routes'
+import Header from './Header'
+import "./styles.css";
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import firebase from 'firebase/app'
+import firebaseConfig from './firebase.config'
+firebase.initializeApp(firebaseConfig)
+
+export const AuthContext = React.createContext(null)
+
+function App(){
+  const[isLoggedIn, setLoggedIn] = useState(false)
+
+  return(
+    <AuthContext.Provider value={{isLoggedIn, setLoggedIn}}>
+      Is logged in? {JSON.stringify(isLoggedIn)} 
+      <div className="App">
+        <Router>
+          <Header />
+
+          <Switch>
+            {routes.map(route=>(
+              <Route 
+              key={route.path}
+              path={route.path}
+              exact={route.exact}
+              component={route.main}
+              />
+            ))}
+          </Switch>
+        </Router>
+      </div>
+    </AuthContext.Provider>
+  )
+}
+
+const rootElement = document.getElementById("root")
+ReactDOM.render(<App/>, rootElement)
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
